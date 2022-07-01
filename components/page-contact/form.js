39602bf14342a7986b27a64173856db4f2deb65e
Evgeniy Makhnin <61658755+evgeniy9286@users.noreply.github.com>
2022-07-01 15:32:17 +0200

    Update form.js

--- components/page-contact/form.js
+++ components/page-contact/form.js
@@ -33,7 +33,7 @@ function Form() {
 	
   return (
   <>
-	  <form>
+	  <Form>
   <div className="form">
 	<input type='text' value={name} onChange={event => setName(event.target.value)} placeholder="Ваше имя"/>
 	<input type='text' value={email} onChange={event => setEmail(event.target.value)} placeholder="Ваш E-mail"/>
@@ -41,7 +41,7 @@ function Form() {
 	<textarea type='text' className='textarea' value={message} onChange={event => setMessage(event.target.value)} placeholder="Опишите ваш проект"/>
 	<button type='button' className='send-form' onClick={sendForm}>Отправить</button>
    </div>
-  </form>
+  </Form>
 	 </>
   )
 }
